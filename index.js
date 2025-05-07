f40c9d511b50f5dc41edb68c71ae1bfa81d4245f
ncr <18fi.i.hryshkovets@std.npu.edu.ua>
2025-05-07 11:12:57 +0300

    final?

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-const PORT = 5000;
-const boardsRouters = require('./Routes/BoardRoutes');
-const authRouters = require('./Routes/AuthRoutes');
-const uploadRouters = require('./Routes/uploadRoutes');
+
 const path = require('path');
 const morgan = require('morgan');
 
@@ -14,15 +11,22 @@ mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB"))
-  	.catch(err => console.log("DB connection error", err));
+    .catch(err => console.log("DB connection error", err));
 
+app.use(morgan('combined'));
 app.use(cors({
     origin: "http://localhost:3000", 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true 
 }));
 app.use(express.json());
+const clientBuildPath = path.resolve(__dirname, 'client', 'build');
+console.log(clientBuildPath);
+app.use(express.static(clientBuildPath)); // Corrected line
 
+const boardsRouters = require('./Routes/BoardRoutes');
+const authRouters = require('./Routes/AuthRoutes');
+const uploadRouters = require('./Routes/uploadRoutes');
 app.use("/api", boardsRouters);
 
 app.use("/api", authRouters);
@@ -30,15 +34,16 @@ app.use("/api", authRouters);
 app.use('/api', uploadRouters);
 
 
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущено на http://localhost:${PORT}`);
+app.get('/*', (req, res, next) => {
+    if (req.originalUrl.startsWith('/api')) {
+        return next(); // Pass the request to the next middleware (e.g., 404 handler)
+    }
+    res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
-const clientBuildPath = path.resolve(__dirname, 'client', 'build');
-console.log("way: ", clientBuildPath);
-app.use(express.static(clientBuildPath));
-app.get('*', (req, res) => {
-	res.sendFile(path.join(clientBuildPath, 'index.html'));
+app.listen(process.env.PORT, () => {
+    console.log(`Сервер запущено на http://localhost:${process.env.PORT}`);
 });
+
+
 
